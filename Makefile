a02431b50361df2825b6ba9a9239ec955341c24f
Egon Willighagen <egon.willighagen@gmail.com>
2019-03-10 12:45:01 +0100

    Also customize the grant number

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: DMP.md
 
 DMP.md: DMP.i.md
-	@groovy createMarkdown.groovy DMP.i.md NanoCommons > DMP.md
+	@groovy createMarkdown.groovy DMP.i.md NanoCommons 731032 > DMP.md
--- createMarkdown.groovy
+++ createMarkdown.groovy
@@ -4,11 +4,14 @@
 
 input = args[0]
 project = args[1]
+grant = args[2]
 
 lines = new File(input).readLines()
 lines.each { String line ->
   if (line.contains("<project/>")) {
     println line.replace("<project/>", project)
+  } else if (line.contains("<grant/>")) {
+    println line.replace("<grant/>", grant)
   } else {
     println line
   }
